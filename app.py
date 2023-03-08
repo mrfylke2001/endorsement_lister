a965237d8315636bb5c8347d7c9b1bf1a1bea00f
Cooper Johnston <cjo113@uit.no>
2023-03-08 14:43:02 +0100

    rewrote with functional patterns

--- app.py
+++ app.py
@@ -1,62 +1,99 @@
 import urllib.request
 import time
 
-nation = input("Enter your nation: ")
-userRequestURL = "https://www.nationstates.net/cgi-bin/api.cgi?nation=" + nation + "&q=region+endorsements"
-userData = str(urllib.request.urlopen(userRequestURL).read())
-userEndoList = (userData.split("<ENDORSEMENTS>"))[1].split("</ENDORSEMENTS>")[0]
-userEndoArray = userEndoList.split(",")
-region = (userData.split("<REGION>"))[1].split("</REGION>")[0].replace(" ", "_").lower()
-print("Got region: " + region)
+def snake_case(x: str):
+    return x.replace(" ", "_").lower()
 
-rRequestURL = "https://www.nationstates.net/cgi-bin/api.cgi?region=" + region + "&q=nations"
-nationsData = str(urllib.request.urlopen(rRequestURL).read())
-nationsList = (nationsData.split("<NATIONS>"))[1].split("</NATIONS>")[0]
+def new_request(url: str) -> urllib.request.Request:
+    USER_AGENT = "Endorsement Lister by mrfylke https://github.com/mrfylke2001/endorsement_lister/"
 
-nArray = nationsList.split(":")
+    request = urllib.request.Request(
+        url,
+        data=None,
+        headers={
+            "User-Agent": USER_AGENT
+        }
+    )
+    return request
 
-nationsWA = []
-unendorsed = []
-for i in range(len(nArray)):
-    print("Testing " + nArray[i])
-    nRequestURL = "https://www.nationstates.net/cgi-bin/api.cgi?nation=" + nArray[i] + "&q=wa+endorsements"
-    nData = str(urllib.request.urlopen(nRequestURL).read())
-    nWAStatus = (nData.split("<UNSTATUS>"))[1].split("</UNSTATUS>")[0]
-    if nWAStatus != "Non-member":
-        print("  " + nArray[i] + " is a WA member.")
-        nationsWA.append(nArray[i])
-        nEndoList = (nData.split("<ENDORSEMENTS>"))[1].split("</ENDORSEMENTS>")[0]
-        nEndoArray = nEndoList.split(",")
-        if nation not in nEndoArray:
-            print("  User has not endorsed " + nArray[i])
-            unendorsed.append(nArray[i])
-    time.sleep(0.7)
+def xml_tag_content(xml: str, tag: str) -> str:
+    tag_contents = xml.split(f"<{tag}>")[1].split(f"</{tag}>")[0]
+    return tag_contents
 
-results = "<!DOCTYPE html><html><body><p>You have not endorsed:</p><ol>"
+def xml_tag_content_list(xml: str, tag: str, delimiter=",") -> list:
+    tag_contents = xml_tag_content(xml, tag)
+    contents_list = tag_contents.split(delimiter)
+    return contents_list
 
-for x in unendorsed:
-    results += f"<li><a href='https://nationstates.net/nation={x}' target='_blank'>{x}</a></li>"
-results += "</ol><p>You have not been endorsed by:</p><ol>"
+def ns_api_url(target_query: str, data_queries: list) -> str:
+    BASE_URL = "https://www.nationstates.net/cgi-bin/api.cgi"
 
-for x in nationsWA:
-    if x not in userEndoArray:
+    data_query_str = "q=" + "+".join(data_queries)
+    url = f"{BASE_URL}?{target_query}&{data_query_str}"
+    return url
+
+def ns_nation_api_url(nation: str, data_queries: list) -> str:
+    target_query = f"nation={nation}"
+    return ns_api_url(target_query, data_queries)
+
+def ns_region_api_url(region: str, data_queries: list) -> str:
+    target_query = f"region={region}"
+    return ns_api_url(target_query, data_queries)
+
+def is_wa_member(api_xml_data: str) -> bool:
+    wa_status = xml_tag_content(api_xml_data, "UNSTATUS")
+    wa_bool = wa_status != "Non-member"
+    return wa_bool
+
+if __name__ == "__main__":
+    user_nation = snake_case(input("Enter your nation: "))
+
+    n_request_url = ns_nation_api_url(user_nation, ["region", "endorsements"])
+    n_request = new_request(n_request_url)
+    n_xml_data = str(urllib.request.urlopen(n_request).read())
+    n_endorsed_by = xml_tag_content_list(n_xml_data, "ENDORSEMENTS")
+
+    region = snake_case(xml_tag_content(n_xml_data, "REGION"))
+    print(f"Got region: {region}")
+
+    r_request_url = ns_region_api_url(region, ["nations"])
+    r_request = new_request(r_request_url)
+    r_xml_data = str(urllib.request.urlopen(r_request).read())
+    r_nations = xml_tag_content_list(r_xml_data, "NATIONS", delimiter=":")
+
+    wa_members = []
+    unendorsed = []
+
+    for nation in r_nations:
+        print(f"Examining: {nation}")
+        request_url = ns_nation_api_url(nation, ["wa", "endorsements"])
+        request = new_request(request_url)
+        xml_data = str(urllib.request.urlopen(request).read())
+
+        if is_wa_member(xml_data):
+            print(f"  {nation} is a WA member.")
+            wa_members.append(nation)
+
+            endorsed_by = xml_tag_content_list(xml_data, "ENDORSEMENTS")
+            if user_nation not in endorsed_by:
+                print(f"  {user_nation} has not endorsed {nation}.")
+                unendorsed.append(nation)
+
+        time.sleep(0.7)
+
+    results = "<!DOCTYPE html><html><body><p>You have not endorsed:</p><ol>"
+
+    for x in unendorsed:
         results += f"<li><a href='https://nationstates.net/nation={x}' target='_blank'>{x}</a></li>"
-results += "</ol></body></html>"
+    results += "</ol><p>You have not been endorsed by:</p><ol>"
 
-with open("results.html", "w") as file:
-    file.write(results)
+    for x in wa_members:
+        if x not in n_endorsed_by:
+            results += f"<li><a href='https://nationstates.net/nation={x}' target='_blank'>{x}</a></li>"
+    results += "</ol></body></html>"
 
-print("================")
-print("Complete")
+    with open("results.html", "w") as file:
+        file.write(results)
 
-#print("You have not endorsed:")
-#for i in range(len(unendorsed)):
-#    print("  " + unendorsed[i])
-
-#print("================")
-#print("You have not been endorsed by:")
-#for i in range(len(nationsWA)):
-#    if nationsWA[i] not in userEndoArray:
-#        print("  " + nationsWA[i])
-
-exiter = input()
+    print("================")
+    print("Complete")
